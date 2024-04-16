cc7eb8e383094b2ef26c772e4e139870a5e4e3ce
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-04-16 18:11:53 +0100

    feat(app): add sliding effet to sider

--- apps/framr-web/components/sideBar/SideBar.tsx
+++ apps/framr-web/components/sideBar/SideBar.tsx
@@ -1,13 +1,13 @@
 import AppFolderIcon from "@iconify-icons/fluent/app-folder-32-regular";
 import ChevronLeft from "@iconify-icons/fluent/chevron-left-32-regular";
-import ChevronRight from "@iconify-icons/fluent/chevron-right-32-regular";
 import TableIcon from "@iconify-icons/fluent/table-32-regular";
 import CommentIcon from "@iconify-icons/fluent/comment-20-regular";
 import { Icon, IconifyIcon } from '@iconify/react';
-import { Box, Divider, Drawer, IconButton, List, Typography } from "@mui/material";
+import { Box, Divider, Drawer, IconButton, List, Typography, useTheme } from "@mui/material";
 import Image from "next/image";
 import { useState } from "react";
-import LogoFramr from "../../public/asset/fullLogoFram.png";
+import FullLogoFramr from "../../public/asset/fullLogoFram.png";
+import ShortLogoFramr from "../../public/asset/shortLogoFrame.png";
 import { useRouter } from "next/router";
 import SideBarNavItem from "./SideBarNavIdem";
 
@@ -22,6 +22,7 @@ export interface sideBarNav {
 export default function SideBar() {
     const [open, setOpen] = useState<boolean>(true)
     const { push } = useRouter()
+    const theme = useTheme();
 
     const sideBarNav: sideBarNav[] = [
         {
@@ -37,38 +38,63 @@ export default function SideBar() {
             icon: AppFolderIcon
         }
     ]
+    const handleOpenSlide = () => {
+        setOpen(false)
+    }
     return (
-        <Drawer variant="permanent" open={open}>
-            <Box sx={{
-                width: 240,
-                padding: '10px 18px',
+        <Drawer variant="permanent" sx={{
+            '.MuiPaper-root': {
                 bgcolor: 'rgba(250, 250, 253, 1)',
+                ...(open && {
+                    width: 240,
+                    overflowX: 'hidden',
+                }),
+                ...(!open && {
+                    width: `calc(${theme.spacing(7)} + 1px)`,
+                    overflowX: 'hidden',
+                    transition: 'width 0.5s ease',
+                }),
+            }
+        }}>
+            <Box sx={{
+                padding: open ? '10px 18px' : '10px',
                 position: 'relative',
-                height: '100%'
+                height: '100%',
             }}>
                 <Box sx={{
                     display: 'flex',
-                    justifyContent: 'space-between',
+                    justifyContent: open ? 'space-between' : 'center',
                     alignItems: 'center',
-                    marginBottom: '15px'
+                    marginBottom: '15px',
                 }}>
-                    <Image
-                        onClick={() => push('/')}
-                        src={LogoFramr}
-                        alt="Framr Logo"
-                        style={{ width: '72px', height: '34.63px', cursor: 'pointer' }}
-                    />
+                    {open ?
+                        <Image
+                            onClick={() => push('/')}
+                            src={FullLogoFramr}
+                            alt="Full Framr Logo"
+                            style={{ width: '72px', height: '34.63px', cursor: 'pointer' }}
+                        /> :
+                        <Image
+                            onClick={() => push('/')}
+                            src={ShortLogoFramr}
+                            alt="Short Framr Logo"
+                            style={{ width: '30px', height: '32.63px', cursor: 'pointer' }}
+                        />
+                    }
                     <IconButton sx={{
-                        boxShadow: '0px 15px 24px 2px rgba(24, 44, 75, 0.08)'
-                    }}>
-                        {open ? <Icon icon={ChevronLeft} style={{ height: '15px', width: '15px' }} /> :
-                            <Icon icon={ChevronRight} style={{ height: '15px', width: '15px' }} />}
+                        boxShadow: '0px 8px 24px 4px rgba(24, 44, 75, 0.08)',
+                        bgcolor: 'rgba(255, 255, 255, 1)',
+                        display: open ? 'inherit' : 'none',
+                    }}
+                        onClick={handleOpenSlide}
+                    >
+                        <Icon icon={ChevronLeft} style={{ height: '15px', width: '15px' }} />
                     </IconButton>
                 </Box>
                 <Divider />
                 <List>
                     {sideBarNav.map((navElements, index) => (
-                        <SideBarNavItem sideBarNav={navElements} key={index} />
+                        <SideBarNavItem sideBarNav={navElements} open={open} key={index} />
                     ))}
                 </List>
                 <Box sx={{
@@ -85,7 +111,8 @@ export default function SideBar() {
                         fontWeight: 600,
                         fontSize: '14px',
                         lineHeight: '14px',
-                        color: 'rgba(110, 109, 122, 1)'
+                        color: 'rgba(110, 109, 122, 1)',
+                        display: open ? 'inherit' : 'none'
 
                     }}>Support</Typography>
 
