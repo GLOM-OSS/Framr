5bd631c56a4b80afa7282733b823cb5fb7483b79
Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-17 09:53:28 +0100

    feat(app): #1 configured font for the project

---
+++ apps/framr-web/pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" type="image/x-icon" href="/favicon.png" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta
+            name="description"
+            content="Framr is a tool that helps you to generate ordered frames for your well drillling projects. with rules management and a lot of features."
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
