8442345fc5668b0b85d925927f2db3aed4407b14
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-04-17 03:03:37 +0100

    feat(app): wrap layout and side bar

--- apps/framr-web/pages/index.tsx
+++ apps/framr-web/pages/index.tsx
@@ -1,10 +1,29 @@
 import { Box } from "@mui/material";
 import SideBar from "../components/sideBar/SideBar";
+import Layout from "../components/layout";
+import { useState } from "react";
 
 export function Index() {
+  const [open, setOpen] = useState<boolean>(true)
+  const [drawerWidth, setDrawerWidth] = useState<number>(240)
+
   return (
-    <Box>
-      <SideBar />
+    <Box sx={{
+      display: "flex",
+    }}>
+      <Layout
+        drawerWidth={drawerWidth}
+        setDrawerWidth={setDrawerWidth}
+        open={open}
+        setOpen={setOpen}
+      >
+        hello
+      </Layout>
+      <SideBar
+        drawerWidth={drawerWidth}
+        setDrawerWidth={setDrawerWidth}
+        open={open}
+        setOpen={setOpen} />
     </Box>
   );
 }
