d971857ad4d5af68811941e682f59d2bd54b2458
Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-17 09:53:56 +0100

    refactor(app): #1 adjusted iconify imports

--- apps/framr-web/components/layout/footer/Footer.tsx
+++ apps/framr-web/components/layout/footer/Footer.tsx
@@ -1,21 +1,18 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography } from '@mui/material';
 import Link from 'next/link';
-import ExternalLink from '@iconify-icons/fluent/open-32-regular'
-import { Icon } from "@iconify/react";
-
+import ExternalLink from '@iconify/icons-fluent/open-32-regular';
+import { Icon } from '@iconify/react';
 
 export default function Footer() {
-    return (
-        <Box>
-            <Typography>Powered by :</Typography>
-            <Link
-                href='#'
-            >
-                <Typography>
-                    GLOM
-                    <Icon icon={ExternalLink} />
-                </Typography>
-            </Link>
-        </Box>
-    );
+  return (
+    <Box>
+      <Typography>Powered by :</Typography>
+      <Link href="#">
+        <Typography>
+          GLOM
+          <Icon icon={ExternalLink} />
+        </Typography>
+      </Link>
+    </Box>
+  );
 }
