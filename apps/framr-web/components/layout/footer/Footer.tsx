8c8cd62a417df92837245819f187a403fdf0a7e6
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-18 20:34:36 +0100

    chore(app): #1 adjusted the footer

--- apps/framr-web/components/layout/footer/Footer.tsx
+++ apps/framr-web/components/layout/footer/Footer.tsx
@@ -1,18 +1,32 @@
-import { Box, Typography } from '@mui/material';
-import Link from 'next/link';
 import ExternalLink from '@iconify/icons-fluent/open-32-regular';
 import { Icon } from '@iconify/react';
+import { Box, Typography } from '@mui/material';
 
 export default function Footer() {
   return (
-    <Box>
-      <Typography>Powered by :</Typography>
-      <Link href="#">
-        <Typography>
-          GLOM
-          <Icon icon={ExternalLink} />
-        </Typography>
-      </Link>
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: 'auto 1fr',
+        alignItems: 'center',
+        columnGap: 0.5,
+      }}
+    >
+      <Typography>Powered by : </Typography>
+      <Box
+        component="a"
+        href="#"
+        target="_blank"
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'auto 1fr',
+          columnGap: 0.5,
+          alignItems: 'center',
+        }}
+      >
+        <Typography>GLOM</Typography>
+        <Icon icon={ExternalLink} />
+      </Box>
     </Box>
   );
 }
