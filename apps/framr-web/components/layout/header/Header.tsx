8abb2f8416d3594b324fd6408f7a5a971bbcc218
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-18 21:38:33 +0100

    feat(app): #1 adjusted the theme switcher on header

--- apps/framr-web/components/layout/header/Header.tsx
+++ apps/framr-web/components/layout/header/Header.tsx
@@ -1,75 +1,80 @@
 import SearchIcon from '@iconify/icons-fluent/search-32-regular';
-import MoonIcon from '@iconify/icons-fluent/weather-moon-28-regular';
-import SunIcon from '@iconify/icons-fluent/weather-sunny-32-regular';
+import MoonIcon from '@iconify/icons-fluent/weather-moon-28-filled';
+import SunIcon from '@iconify/icons-fluent/weather-sunny-32-filled';
 import { Icon } from '@iconify/react';
 import {
-  Autocomplete,
   Box,
+  IconButton,
   InputAdornment,
-  Switch,
   TextField,
   Typography,
 } from '@mui/material';
+import { useState } from 'react';
+import IOSSwitch from '../../sharedComponents/iosSwitch';
 
 export default function Header() {
+  const [isLightMode, setIsLightMode] = useState<boolean>(false);
   return (
     <Box
       sx={{
-        position: 'relative',
         bgcolor: 'rgba(250, 250, 253, 1)',
         display: 'flex',
         width: '100%',
         justifyContent: 'space-between',
         alignItems: 'center',
-        height: 80,
+        padding: '10px 20px',
       }}
     >
-      <Box>
-        <Typography
-          sx={{
-            fontFamily: 'inter',
-            fontWeight: 500,
-            fontSize: '12px',
-            lineHeight: '16px',
-            paddingLeft: '40px',
-          }}
-        >
-          Configuration/tool/
-        </Typography>
-      </Box>
-      <Autocomplete
-        options={[]}
+      <Typography
         sx={{
-          width: 400,
+          fontFamily: 'inter',
+          fontWeight: 500,
+          fontSize: '12px',
+          lineHeight: '16px',
+          paddingLeft: '40px',
         }}
+      >
+        Configuration/tool/
+      </Typography>
+      <TextField
+        fullWidth
+        variant="outlined"
+        placeholder="Search Tools"
         size="small"
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            fullWidth
-            variant="outlined"
-            placeholder="Search Tools"
-            InputProps={{
-              ...params.InputProps,
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Icon icon={SearchIcon} />
-                </InputAdornment>
-              ),
-            }}
-          />
-        )}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Icon icon={SearchIcon} />
+            </InputAdornment>
+          ),
+        }}
       />
       <Box
         sx={{
-          display: 'flex',
+          display: 'grid',
           alignItems: 'center',
-          paddingRight: '10px',
+          gridAutoFlow: 'column',
+          columnGap: 1,
         }}
       >
-        <Icon icon={MoonIcon} />
-        <Switch />
-        <Icon icon={SunIcon} />
+        <IconButton size="small" onClick={() => setIsLightMode(false)}>
+          <Icon
+            icon={MoonIcon}
+            fontSize={24}
+            color={!isLightMode ? '#2F3A45' : '#D1D5DB'}
+          />
+        </IconButton>
+        <IOSSwitch
+          checked={isLightMode}
+          onChange={(event) => setIsLightMode(event.target.checked)}
+        />
+        <IconButton size="small" onClick={() => setIsLightMode(true)}>
+          <Icon
+            icon={SunIcon}
+            fontSize={24}
+            color={isLightMode ? '#3B82F6' : '#D1D5DB'}
+          />
+        </IconButton>
       </Box>
     </Box>
   );
