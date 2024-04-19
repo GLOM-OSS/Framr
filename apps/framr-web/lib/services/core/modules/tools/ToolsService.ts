352001720875bdbe85344ae58aec15e9f77bf0c5
Kuidja Marco <61763373+Marcjazz@users.noreply.github.com>
2024-04-19 18:55:56 +0100

    Emmanuel tientcheu feature/1 tools crud (#25)
    * ci(app): #1 installed mui dependencies
    
    * feat(app): #1 added mui theme to project
    
    * ci(shared): #1 added iconify to workspace
    
    * feat(app): 1 crud on the tools object
    
    This concerns the operation creation edition deletion and modification carried out on the tools object
    
    * ci(app): #1 installed mui dependencies
    
    * feat(app): #1 added mui theme to project
    
    * ci(shared): #1 added iconify to workspace
    
    * feat(shared): remove all unused code
    
    * chore(shared): install mui, better-commits libraries
    
    * chore(app): add and rename framr logo
    
    * feat(app): add framr logo as favicon
    
    * feat(app): set up side bar
    
    * feat(app): set up navigation item for side bar
    
    * feat(app): add side bar comp to index racine view
    
    * feat(app): remove style css from _app
    
    * feat(app): add sliding side par
    
    * feat(app): add sliding effet to sider
    
    * fix(app): rename handle close side bar function
    
    * feat(app): wrap header and footer
    
    * feat(app): setup header
    
    * feat(app): set up footer
    
    * feat(app): add dynamic with to make the side bar moving
    
    * feat(app): wrap layout and side bar
    
    * fix(db): #3 added complementory types
    
    * fix(db): #3 updated service layer structure (possible breaking changes)
    
    * feat(app): 1 moving the tools service into the core module
    
    moving the tools service into the core module and exporting from the index
    
    * fix(app): 1 resolve merge conflits
    
    resolve merges conflits in ToolsService
    
    * chore(app): 1 add prettier format in package.json
    
    add prettier format in script on package.json
    
    * fix(app): 1 formated service file with prettier
    
    formated service file with prettier
    
    * refactor(app): #1 refeactor creat tools method
    
    * refactor(app): #1 refactor findOne method in ToolsService
    
    refactor findOne method in ToolsService
    
    * refactor(app): #1 refactor findAll method in ToolsService
    
    refactor findAll method in ToolsService
    
    * refactor(app): #1 refactor update method in ToolsService
    
    refactor update method in ToolsService
    
    * refactor(app): #1 refactor delete method in ToolsService
    
    refactor delete method in ToolsService
    
    * refactor(app): #1 refactor crud mathod in ToolsService
    
    refactor crud metho in ToolsService
    
    * refactor(app): #1 refactor crud mwthod in ToolsService
    
    refactor crud method in ToolService
    
    * fix(db): #4 reviewed and fix tools management service class
    
    * chore(db): #4 deleted environmental files
    
    ---------
    
    Co-authored-by: Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
    Co-authored-by: emmanuel-tientcheu <tientcheutchokouatoue@gmail.com>
    Co-authored-by: Poukam-Pierre <ngamaleu2011@gmail.com>
    Co-authored-by: emmanuel-tientcheu <89963864+emmanuel-tientcheu@users.noreply.github.com>

---
+++ apps/framr-web/lib/services/core/modules/tools/ToolsService.ts
@@ -0,0 +1,128 @@
+import { CreateTool, MWDTool } from '../../../../types';
+import { ToolEnum } from '../../../../types/enums';
+import { FramrServiceError } from '../../../libs/errors';
+import {
+  EventBus,
+  EventBusChannelStatus,
+  EventBusPayload,
+} from '../../../libs/event-bus';
+import { IDBFactory } from '../../../libs/idb';
+import { IDBConnection } from '../../db/IDBConnection';
+import { FramrDBSchema, ToolRecord } from '../../db/schema';
+import { ToolInterface, ToolsEventChannel } from './ToolInterface';
+
+export class ToolsService implements ToolInterface {
+  private readonly eventBus: EventBus;
+  private readonly database: IDBFactory<FramrDBSchema>;
+  private readonly STORE_NAME = 'tools';
+
+  constructor() {
+    this.database = IDBConnection.getDatabase();
+    this.eventBus = new EventBus();
+  }
+
+  create(createTool: CreateTool): void {
+    const channel = ToolsEventChannel.CREATE_TOOLS_CHANNEL;
+    const newTool: ToolRecord = {
+      value: {
+        id: crypto.randomUUID(),
+        ...(createTool.type === ToolEnum.MWD
+          ? { ...(createTool as MWDTool), type: ToolEnum.MWD }
+          : { ...createTool, type: ToolEnum.LWD }),
+      },
+    };
+
+    this.database
+      .insert(this.STORE_NAME, newTool)
+      .then((response) => {
+        this.eventBus.emit(channel, {
+          data: { ...newTool!.value, id: response },
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  findOne(index: number): void {
+    const channel = ToolsEventChannel.FIND_ONE_TOOLS_CHANNEL;
+
+    this.database
+      .findOne(this.STORE_NAME, index)
+      .then((response) => {
+        const payload: EventBusPayload<EventBusChannelStatus> = response
+          ? { data: response.value, status: EventBusChannelStatus.SUCCESS }
+          : {
+              data: new FramrServiceError('Tool not found'),
+              status: EventBusChannelStatus.ERROR,
+            };
+        this.eventBus.emit(channel, payload);
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  findAll(): void {
+    const channel = ToolsEventChannel.FIND_ALL_TOOLS_CHANNEL;
+    this.database
+      .findAll(this.STORE_NAME)
+      .then((response) => {
+        this.eventBus.emit(channel, {
+          data: response.map((_) => _.value),
+          status: EventBusChannelStatus.ERROR,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  update(index: number, createTool: CreateTool): void {
+    const channel = ToolsEventChannel.UPDATE_TOLLS_CHANNEL;
+
+    this.database
+      .update(this.STORE_NAME, index, createTool)
+      .then(() => {
+        this.eventBus.emit(channel, {
+          data: undefined,
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  delete(index: number): void {
+    const channel = ToolsEventChannel.DELETE_TOLLS_CHANNEL;
+
+    this.database
+      .delete(this.STORE_NAME, index)
+      .then(() => {
+        this.eventBus.emit(channel, {
+          data: undefined,
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+}
--- apps/framr-web/lib/services/libs/event-bus/EventBus.types.d.ts
+++ apps/framr-web/lib/services/libs/event-bus/EventBus.types.d.ts
@@ -1,5 +1,5 @@
 import { FramrServiceError } from '../errors';
-import { EventBusChannelStatus } from './EventBus.enum';
+import { EventBusChannelStatus } from './EventBusEnum';
 
 type EventBusDataMap = {
   [EventBusChannelStatus.SUCCESS]: unknown;
