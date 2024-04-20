e597e715c707855e622b4334fdc7964685bbf338
emmanuel-tientcheu <89963864+emmanuel-tientcheu@users.noreply.github.com>
2024-04-21 00:44:38 +0100

    Feat/#6 dpoints crud (#27)
    * ci(app): #1 installed mui dependencies
    
    * feat(app): #1 added mui theme to project
    
    * ci(shared): #1 added iconify to workspace
    
    * feat(app): 1 crud on the tools object
    
    This concerns the operation creation edition deletion and modification carried out on the tools object
    
    * ci(app): #1 installed mui dependencies
    
    * feat(app): #1 added mui theme to project
    
    * ci(shared): #1 added iconify to workspace
    
    * feat(shared): remove all unused code
    
    * chore(shared): install mui, better-commits libraries
    
    * chore(app): add and rename framr logo
    
    * feat(app): add framr logo as favicon
    
    * feat(app): set up side bar
    
    * feat(app): set up navigation item for side bar
    
    * feat(app): add side bar comp to index racine view
    
    * feat(app): remove style css from _app
    
    * feat(app): add sliding side par
    
    * feat(app): add sliding effet to sider
    
    * fix(app): rename handle close side bar function
    
    * feat(app): wrap header and footer
    
    * feat(app): setup header
    
    * feat(app): set up footer
    
    * feat(app): add dynamic with to make the side bar moving
    
    * feat(app): wrap layout and side bar
    
    * fix(db): #3 added complementory types
    
    * fix(db): #3 updated service layer structure (possible breaking changes)
    
    * feat(app): 1 moving the tools service into the core module
    
    moving the tools service into the core module and exporting from the index
    
    * fix(app): 1 resolve merge conflits
    
    resolve merges conflits in ToolsService
    
    * chore(app): 1 add prettier format in package.json
    
    add prettier format in script on package.json
    
    * fix(app): 1 formated service file with prettier
    
    formated service file with prettier
    
    * refactor(app): #1 refeactor creat tools method
    
    * refactor(app): #1 refactor findOne method in ToolsService
    
    refactor findOne method in ToolsService
    
    * refactor(app): #1 refactor findAll method in ToolsService
    
    refactor findAll method in ToolsService
    
    * refactor(app): #1 refactor update method in ToolsService
    
    refactor update method in ToolsService
    
    * refactor(app): #1 refactor delete method in ToolsService
    
    refactor delete method in ToolsService
    
    * refactor(app): #1 refactor crud mathod in ToolsService
    
    refactor crud metho in ToolsService
    
    * refactor(app): #1 refactor crud mwthod in ToolsService
    
    refactor crud method in ToolService
    
    * feat(app): #2 add Dpoint interface and service
    
    implement around a Dpoint object
    
    * refactor(app): #1 refactor ToolService
    
    refactorToolService
    
    * refactor(app): #2 refactor crud method in DpointService
    
    refactorMethod in DpointService
    
    * refactor(app): #6 refactor crud method in DpointsService
    
    * refactor(app): #6 Change name Dpoint to DPoint in interface and service
    
    ---------
    
    Co-authored-by: Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
    Co-authored-by: Poukam-Pierre <ngamaleu2011@gmail.com>
    Co-authored-by: Marco Kuidja <kuidjamarco@gmail.com>

---
+++ apps/framr-web/lib/services/core/modules/dpoints/DPointsService.ts
@@ -0,0 +1,125 @@
+import { CreateDPoint } from 'apps/framr-web/lib/types';
+import { FramrServiceError } from '../../../libs/errors';
+import {
+  EventBus,
+  EventBusChannelStatus,
+  EventBusPayload,
+} from '../../../libs/event-bus';
+import { IDBFactory, StoreRecord } from '../../../libs/idb';
+import { IDBConnection } from '../../db/IDBConnection';
+import { DPointRecord, FramrDBSchema } from '../../db/schema';
+import { DPointInterface, DPointsEventChannel } from './DPointInterface';
+
+export class DPointsService implements DPointInterface {
+  private readonly eventBus: EventBus;
+  private readonly database: IDBFactory<FramrDBSchema>;
+  private readonly STORE_NAME = 'dpoints';
+
+  constructor() {
+    this.database = IDBConnection.getDatabase();
+    this.eventBus = new EventBus();
+  }
+
+  create(createDpoint: CreateDPoint): void {
+    const channel = DPointsEventChannel.CREATE_DPOINT_CHANNEL;
+    const newDpoint: DPointRecord = {
+      value: {
+        id: crypto.randomUUID(),
+        ...createDpoint,
+      },
+    };
+
+    this.database
+      .insert(this.STORE_NAME, newDpoint)
+      .then((response) => {
+        this.eventBus.emit(channel, {
+          data: { ...newDpoint!.value, id: response },
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  findOne(index: number): void {
+    const channel = DPointsEventChannel.FIND_ONE_DPOINT_CHANNEL;
+
+    this.database
+      .findOne(this.STORE_NAME, index)
+      .then((response) => {
+        const payload: EventBusPayload<EventBusChannelStatus> = response
+          ? { data: response.value, status: EventBusChannelStatus.SUCCESS }
+          : {
+              data: new FramrServiceError('Dpoint not found'),
+              status: EventBusChannelStatus.ERROR,
+            };
+        this.eventBus.emit(channel, payload);
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  findAll(): void {
+    const channel = DPointsEventChannel.FIND_ALL_DPOINT_CHANNEL;
+    this.database
+      .findAll(this.STORE_NAME)
+      .then((response) => {
+        this.eventBus.emit(channel, {
+          data: response.map((_) => _.value),
+          status: EventBusChannelStatus.ERROR,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  update(index: number, createDpoint: CreateDPoint): void {
+    const channel = DPointsEventChannel.UPDATE_DPOINT_CHANNEL;
+
+    this.database
+      .update(this.STORE_NAME, index, createDpoint)
+      .then(() => {
+        this.eventBus.emit(channel, {
+          data: undefined,
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  delete(index: number): void {
+    const channel = DPointsEventChannel.DELETE_DPOINT_CHANNEL;
+
+    this.database
+      .delete(this.STORE_NAME, index)
+      .then(() => {
+        this.eventBus.emit(channel, {
+          data: undefined,
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+}
