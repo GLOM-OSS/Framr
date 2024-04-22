37febafcfe655955b5723cb0fea71d0eeceb1bd0
emmanuel-tientcheu <tientcheutchokouatoue@gmail.com>
2024-04-22 07:02:36 +0100

    feat(app): #7 implement crud for service object
    implement crud for service object

--- apps/framr-web/lib/services/core/modules/index.ts
+++ apps/framr-web/lib/services/core/modules/index.ts
@@ -1,3 +1,4 @@
 export * from './tools/ToolsService';
 export * from './dpoints/DPointsService';
 export * from './rules/RulesService';
+export * from './services/ServicesService';
---
+++ apps/framr-web/lib/services/core/modules/services/ServicesService.ts
@@ -0,0 +1,128 @@
+import { CreateService } from 'apps/framr-web/lib/types';
+import { FramrServiceError } from '../../../libs/errors';
+import {
+  EventBus,
+  EventBusChannelStatus,
+  EventBusPayload,
+} from '../../../libs/event-bus';
+import { IDBFactory } from '../../../libs/idb';
+import { IDBConnection } from '../../db/IDBConnection';
+import { FramrDBSchema, ServiceRecord } from '../../db/schema';
+import { ServiceInterface, ServicesEventChannel } from './ServiceInterface';
+
+
+export class ServicesService implements ServiceInterface {
+  private readonly eventBus: EventBus;
+  private readonly database: IDBFactory<FramrDBSchema>;
+  private readonly STORE_NAME = 'services';
+
+  constructor() {
+    this.database = IDBConnection.getDatabase();
+    this.eventBus = new EventBus();
+  }
+
+  create(createService: CreateService): void {
+    const channel = ServicesEventChannel.CREATE_SERVICES_CHANNEL;
+    const newRule: ServiceRecord = {
+      value: {
+        id: crypto.randomUUID(),
+        ...createService,
+      },
+    };
+
+    this.database
+      .insert(this.STORE_NAME, newRule)
+      .then((response) => {
+        this.eventBus.emit(channel, {
+          data: { ...newRule!.value, id: response },
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  findOne(index: string): void {
+    const channel = ServicesEventChannel.FIND_ONE_SERVICES_CHANNEL;
+
+    this.database
+      .findOne(this.STORE_NAME, index)
+      .then((response) => {
+        const payload: EventBusPayload<EventBusChannelStatus> = response
+          ? { data: response.value, status: EventBusChannelStatus.SUCCESS }
+          : {
+              data: new FramrServiceError('Service not found'),
+              status: EventBusChannelStatus.ERROR,
+            };
+        this.eventBus.emit(channel, payload);
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  findAll(): void {
+    const channel = ServicesEventChannel.FIND_ALL_SERVICES_CHANNEL;
+    this.database
+      .findAll(this.STORE_NAME)
+      .then((response) => {
+        this.eventBus.emit(channel, {
+          data: response.map((_) => _.value),
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+  update(index: string, createService: CreateService): void {
+    const channel = ServicesEventChannel.UPDATE_SERVICES_CHANNEL;
+
+    this.database
+      .update(this.STORE_NAME, index, createService)
+      .then(() => {
+        this.eventBus.emit(channel, {
+          data: undefined,
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+
+  delete(index: string): void {
+    const channel = ServicesEventChannel.DELETE_SERVICES_CHANNEL;
+
+    this.database
+      .delete(this.STORE_NAME, index)
+      .then(() => {
+        this.eventBus.emit(channel, {
+          data: undefined,
+          status: EventBusChannelStatus.SUCCESS,
+        });
+      })
+      .catch((error) => {
+        this.eventBus.emit(channel, {
+          data: new FramrServiceError(error.message),
+          status: EventBusChannelStatus.ERROR,
+        });
+      });
+  }
+
+}
