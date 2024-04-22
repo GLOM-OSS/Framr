1caff1b29ef6816aa916da1b604ce85031fceb0f
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-04-22 11:20:09 +0100

    feat(app): setup modal component for a whole project

---
+++ apps/framr-web/components/modal/Modal.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react";
+import { Modal as BaseModal } from '@mui/base/Modal';
+
+export default function Modal({ children }: PropsWithChildren) {
+    return (
+        <div>
+            general modal will setup here taken chold component
+        </div>
+    );
+}
