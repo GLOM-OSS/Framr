2d76070e95238fc32006428aa2efdd74b25fb79c
Marco Kuidja <kuidjamarco@gmail.com>
2024-04-27 09:06:12 +0100

    feat(service-layer): #34 implemented dpoints dispatching into various framesets

--- apps/framr-web/lib/services/core/modules/framer/framer.service.ts
+++ apps/framr-web/lib/services/core/modules/framer/framer.service.ts
@@ -1,10 +1,11 @@
-import { FrameEnum } from '../../../../../lib/types/enums';
 import {
   CreateGeneratorConfig,
+  DPoint,
   FramesetDpoint,
   GeneratorConfig,
-  GeneratorConfigRule,
+  GeneratorConfigRule
 } from '../../../../../lib/types';
+import { FrameEnum, RuleEnum } from '../../../../../lib/types/enums';
 import { FramrServiceError } from '../../../libs/errors';
 import { EventBus } from '../../../libs/event-bus';
 import { IDBFactory } from '../../../libs/idb';
@@ -26,6 +27,8 @@ export class FramerService {
     if (!this.generatorConfig) {
       const dpoints = await this.database.findAll('dpoints');
       const rules = await this.database.findAll('rules');
+      const services = await this.database.findAll('services');
+
       const utilRules = rules.filter((_) =>
         _.value.framesets.includes(FrameEnum.UTIL)
       );
@@ -33,7 +36,14 @@ export class FramerService {
         ...config,
         id: crypto.randomUUID(),
         framesets: {
-          fsl: [],
+          fsl: [...new Array(6)].map((_, i) => ({
+            framesets: {
+              [FrameEnum.GTF]: { dpoints: [], frame: FrameEnum.GTF },
+              [FrameEnum.MTF]: { dpoints: [], frame: FrameEnum.GTF },
+              [FrameEnum.ROT]: { dpoints: [], frame: FrameEnum.GTF },
+            },
+            number: i + 1,
+          })),
           utility: {
             frame: FrameEnum.UTIL,
             dpoints: dpoints
@@ -51,6 +61,9 @@ export class FramerService {
         },
         tools: config.tools.map((tool) => ({
           ...tool,
+          services: services
+            .filter((_) => _.value.tool.id === tool.id)
+            .map((_) => _.value),
           rules: rules
             .filter(({ value: rule }) => rule.tool.id === tool.id)
             .map<GeneratorConfigRule>((_) => ({
@@ -61,5 +74,59 @@ export class FramerService {
         })),
       };
     } else throw new FramrServiceError('Service was already initialized');
+  }
+
+  async updateFslFramesets(fslNumber: number, newDpoints: DPoint[]) {
+    if (!this.generatorConfig) {
+      throw new FramrServiceError('Service was not initialized');
+    }
+
+    if (fslNumber > this.generatorConfig.framesets.fsl.length) {
+      throw new FramrServiceError('Incorrect Fsl number');
+    }
+
+    const rules = await this.database.findAll('rules');
+    const fslInstances = this.generatorConfig.framesets.fsl;
+    const currentFSL = fslInstances.find((_) => _.number === fslNumber);
+    if (!currentFSL) {
+      throw new FramrServiceError('Could not find Fsl instance');
+    }
+
+    for (const dpoint of newDpoints) {
+      const dpointRule = rules.find(
+        (_) =>
+          _.value.concernedDpoint.id === dpoint.id &&
+          _.value.description === RuleEnum.SHOULD_BE_PRESENT
+      );
+      if (dpointRule) {
+        const { framesets } = dpointRule.value;
+        for (const frame of framesets) {
+          if (
+            frame === FrameEnum.MTF ||
+            frame === FrameEnum.ROT ||
+            frame === FrameEnum.GTF
+          ) {
+            const currentFrameset = currentFSL.framesets[frame];
+            currentFSL.framesets[frame] = {
+              frame: currentFrameset.frame,
+              dpoints: [
+                ...currentFrameset.dpoints,
+                { ...dpoint, isBaseInstance: true },
+              ],
+            };
+          } else if (frame === FrameEnum.UTIL) {
+            this.generatorConfig.framesets.utility.dpoints.push({
+              ...dpoint,
+              isBaseInstance: true,
+            });
+          }
+        }
+      }
+    }
+
+    const isFSLPresent = fslInstances.some((_) => _.number === fslNumber);
+    this.generatorConfig.framesets.fsl = isFSLPresent
+      ? fslInstances.map((fsl) => (fsl.number === fslNumber ? currentFSL : fsl))
+      : [currentFSL];
   }
 }
