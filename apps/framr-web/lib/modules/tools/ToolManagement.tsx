ca6c99c9ebd5d744fb0c4bd1d54657c8ddc25c33
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-05-03 02:07:26 +0100

    refactor(framr-web): #31 added tool listing component

--- apps/framr-web/lib/modules/tools/ToolManagement.tsx
+++ apps/framr-web/lib/modules/tools/ToolManagement.tsx
@@ -1,160 +1,207 @@
 import AddIcon from '@iconify/icons-fluent/add-24-filled';
 import AttachIcon from '@iconify/icons-fluent/attach-24-filled';
-import DeleteIcon from '@iconify/icons-fluent/delete-24-regular';
-import EditIcon from '@iconify/icons-fluent/edit-20-regular';
 import more from '@iconify/icons-fluent/more-vertical-24-regular';
-import OrganizationIcon from '@iconify/icons-fluent/organization-24-regular';
-import SettingIcon from '@iconify/icons-fluent/settings-cog-multiple-24-regular';
-import RulesIcon from '@iconify/icons-fluent/textbox-settings-24-regular';
 import { Icon } from '@iconify/react';
-import {
-  Box,
-  Button,
-  IconButton,
-  Menu,
-  Tooltip,
-  Typography,
-} from '@mui/material';
+import { Box, Button, IconButton, Tooltip, Typography } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useState } from 'react';
+import { ConfirmDialog } from '../../../components/sharedComponents/confirmDialog';
 import { theme } from '../../theme';
-import { LWDTool } from '../../types';
+import { CreateTool, ITool, Tool } from '../../types';
 import { ToolEnum } from '../../types/enums';
-import ToolMoreMenu, { iconMenuTool } from './ToolMoreMenu';
-
-const columns: GridColDef[] = [
-  { field: 'name', headerName: 'Tools Name', width: 300 },
-  { field: 'version', headerName: 'Version', width: 300 },
-  { field: 'long', headerName: 'Long', width: 570 },
-];
-const dataTableGrid: LWDTool[] = [
-  {
-    id: 'abcd123',
-    name: 'ADN',
-    version: 'V8.5bf8',
-    long: 'adnVISION 675',
-    type: ToolEnum.LWD,
-  },
-  {
-    id: 'abcd132',
-    name: 'ADN',
-    version: 'V8.5bf8',
-    long: 'adnVISION 675',
-    type: ToolEnum.LWD,
-  },
-  {
-    id: 'abcd213',
-    name: 'ADN',
-    version: 'V8.5bf8',
-    long: 'adnVISION 675',
-    type: ToolEnum.LWD,
-  },
-  {
-    id: 'abcd231',
-    name: 'ADN',
-    version: 'V8.5bf8',
-    long: 'adnVISION 675',
-    type: ToolEnum.LWD,
-  },
-  {
-    id: 'abcd321',
-    name: 'ADN',
-    version: 'V8.5bf8',
-    long: 'adnVISION 675',
-    type: ToolEnum.LWD,
-  },
-  {
-    id: 'abcd312',
-    name: 'ADN',
-    version: 'V8.5bf8',
-    long: 'adnVISION 675',
-    type: ToolEnum.LWD,
-  },
-];
+import ManageToolDialog from './ManageToolDialog';
+import MoreMenu from './MoreMenu';
 
 export default function ToolManagement() {
-  const [open, setOpen] = useState<boolean>(false);
-  const actionColumns: GridColDef[] = [
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  //TODO; replace this with a call to the API
+  const [tools] = useState<Tool[]>([
+    {
+      id: 'abcd123',
+      name: 'ADN',
+      version: 'V8.5bf8',
+      long: 'adnVISION 675',
+      type: ToolEnum.LWD,
+    },
+    {
+      id: 'abcd132',
+      name: 'ADN',
+      version: 'V8.5bf8',
+      long: 'adnVISION 675',
+      type: ToolEnum.LWD,
+    },
+    {
+      id: 'abcd213',
+      name: 'ADN',
+      version: 'V8.5bf8',
+      long: 'adnVISION 675',
+      type: ToolEnum.LWD,
+    },
+    {
+      id: 'abcd231',
+      name: 'ADN',
+      version: 'V8.5bf8',
+      long: 'adnVISION 675',
+      type: ToolEnum.LWD,
+    },
+    {
+      id: 'abcd321',
+      name: 'ADN',
+      version: 'V8.5bf8',
+      long: 'adnVISION 675',
+      type: ToolEnum.LWD,
+    },
+    {
+      id: 'abcd312',
+      name: 'ADN',
+      version: 'V8.5bf8',
+      long: 'adnVISION 675',
+      type: ToolEnum.LWD,
+    },
+  ]);
+
+  const [activeTool, setActiveTool] = useState<Tool>();
+
+  const toolColumns: GridColDef<Tool>[] = [
+    { field: 'version', headerName: 'Version', width: 100 },
+    { field: 'name', headerName: 'Tools Name', flex: 1 },
+    { field: 'long', headerName: 'Long', flex: 1 },
     {
       field: 'action',
       headerName: '',
       width: 50,
-      renderCell: () => (
+      renderCell: ({ row: tool }) => (
         <Tooltip title="More">
-          <IconButton onClick={handleClick}>
-            <Icon icon={more} fontSize={20} />
+          <IconButton
+            onClick={(event) => {
+              setActiveTool(tool);
+              setAnchorEl(event.currentTarget);
+            }}
+          >
+            <Icon icon={more} fontSize={18} />
           </IconButton>
         </Tooltip>
       ),
     },
   ];
-  const handleClick = () => {
-    setOpen((open) => !open);
-  };
 
-  const elementsMenu: iconMenuTool[] = [
-    { text: 'Manage Rules', icon: RulesIcon },
-    { text: 'Manage Data Point', icon: OrganizationIcon },
-    { text: 'Manage Services', icon: SettingIcon },
-    { text: 'Edit', icon: EditIcon },
-    { text: 'Details', icon: more },
-    { text: 'Delete', icon: DeleteIcon, stateColor: 'red' },
-  ];
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const [isDetailsDialogOpen, setIsDetailsDialogOpen] = useState(false);
+  const [isCreateNewToolDialogOpen, setIsCreateNewToolDialogOpen] =
+    useState<boolean>(false);
+
+  function handleCreateTool(val: CreateTool) {
+    //TODO: CALL API HERE TO CREATE NEW TOOL
+    console.log(val);
+  }
+
+  function handleEditTool(val: ITool) {
+    //TODO: CALL API HERE TO EDIT NEW TOOL
+    console.log(val);
+  }
+
   return (
-    <Box
-      sx={{
-        height: '100%',
-        display: 'grid',
-        gridTemplateRows: 'auto 1fr',
-        rowGap: 1,
-      }}
-    >
+    <>
+      <ManageToolDialog
+        closeDialog={() => {
+          setIsCreateNewToolDialogOpen(false);
+          setIsEditDialogOpen(false);
+          setActiveTool(undefined);
+        }}
+        isDialogOpen={isEditDialogOpen || isCreateNewToolDialogOpen}
+        isSubmitting={false}
+        handleCreate={handleCreateTool}
+        handleEdit={handleEditTool}
+        data={activeTool}
+      />
+      {activeTool && (
+        <>
+          <MoreMenu
+            anchorEl={anchorEl}
+            isMenuOpen={!!anchorEl}
+            closeMenu={() => setAnchorEl(null)}
+            activeTool={activeTool}
+            handleEdit={() => setIsEditDialogOpen(true)}
+            handleDetails={() => setIsDetailsDialogOpen(true)}
+            handleDelete={() => setIsDeleteDialogOpen(true)}
+          />
+          <ConfirmDialog
+            isDialogOpen={isDeleteDialogOpen}
+            closeDialog={() => setIsDeleteDialogOpen(false)}
+            confirm={() => {
+              //TODO: CALL API HERE TO DELETE
+              alert('delete tool');
+              setIsDeleteDialogOpen(false);
+            }}
+            dialogMessage="This action will delete the selected tool. You won't be able to recover it."
+            dialogTitle="Are you sure?"
+            confirmButton="Yes, delete tool"
+            danger
+            closeOnConfirm
+          />
+        </>
+      )}
       <Box
         sx={{
+          height: '100%',
           display: 'grid',
-          alignItems: 'center',
-          gridTemplateColumns: 'auto 1fr',
-          justifyItems: 'end',
-          columnGap: 1,
+          gridTemplateRows: 'auto 1fr',
+          rowGap: 1,
         }}
       >
         <Box
           sx={{
             display: 'grid',
-            gridAutoFlow: 'column',
             alignItems: 'center',
+            gridTemplateColumns: 'auto 1fr',
+            justifyItems: 'end',
             columnGap: 1,
           }}
         >
-          <Typography variant="h3">Tools</Typography>
-          <Typography variant="h3" sx={{ color: theme.common.line }}>
-            {dataTableGrid.length}
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: 'grid',
-            gridAutoFlow: 'column',
-            columnGap: 1,
-          }}
-        >
-          <Button
-            variant="outlined"
-            color="inherit"
-            startIcon={<Icon icon={AttachIcon} />}
+          <Box
+            sx={{
+              display: 'grid',
+              gridAutoFlow: 'column',
+              alignItems: 'center',
+              columnGap: 1,
+            }}
           >
-            Import Tool
-          </Button>
-          <Button variant="contained" startIcon={<Icon icon={AddIcon} />}>
-            Create Tool
-          </Button>
+            <Typography variant="h3">Tools</Typography>
+            <Typography variant="h3" sx={{ color: theme.common.line }}>
+              {tools.length}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'grid',
+              gridAutoFlow: 'column',
+              columnGap: 1,
+            }}
+          >
+            <Button
+              variant="outlined"
+              color="inherit"
+              startIcon={<Icon icon={AttachIcon} />}
+            >
+              Import Tool
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => setIsCreateNewToolDialogOpen(true)}
+              startIcon={<Icon icon={AddIcon} />}
+            >
+              Create Tool
+            </Button>
+          </Box>
         </Box>
-      </Box>
-      <Box>
         <DataGrid
-          rows={dataTableGrid}
-          columns={columns.concat(actionColumns)}
+          rows={tools}
+          columns={toolColumns}
           autoHeight
+          hideFooter
+          autoPageSize
           disableColumnMenu
           hideFooterSelectedRowCount
           sx={{
@@ -168,47 +215,7 @@ export default function ToolManagement() {
             },
           }}
         />
-
-        <Menu
-          elevation={0}
-          anchorOrigin={{
-            vertical: 'center',
-            horizontal: 'right',
-          }}
-          sx={{
-            '& .MuiPaper-root': {
-              borderRadius: '10px',
-              marginTop: '8px',
-              minWidth: 180,
-              color: 'rgb(55, 65, 81)',
-              boxShadow:
-                'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-              '& .MuiMenu-list': {
-                padding: '4px 0',
-              },
-              '& .MuiMenuItem-root': {
-                '& .MuiSvgIcon-root': {
-                  fontSize: 18,
-                  marginRight: '12px',
-                },
-              },
-              '& .css-h4y409-MuiList-root': {
-                padding: '4px 0',
-              },
-            },
-          }}
-          open={open}
-          onClose={handleClick}
-        >
-          {elementsMenu.map((elementMenu, index) => (
-            <ToolMoreMenu
-              key={index}
-              handleClick={handleClick}
-              elementMenu={elementMenu}
-            />
-          ))}
-        </Menu>
       </Box>
-    </Box>
+    </>
   );
 }
