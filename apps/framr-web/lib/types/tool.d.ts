41c7dc7d43def22ab5497dd7fd83fa8b44160d7c
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-05-03 02:06:27 +0100

    refactor(framr-web): #31 adjusted types for creation

--- apps/framr-web/lib/types/tool.d.ts
+++ apps/framr-web/lib/types/tool.d.ts
@@ -1,5 +1,18 @@
 import { ToolEnum } from './enums';
 
+export interface CreateTool {
+  name: string;
+  version: string;
+  long: string;
+  type: ToolEnum;
+  max_bits?: number;
+  max_dpoints?: number;
+}
+
+export interface ITool extends CreateTool {
+  id: string;
+}
+
 export interface CreateLWDTool {
   name: string;
   version: string;
