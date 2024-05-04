038c299930947f8ac3b5f4967c7be106813cc01e
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-05-04 13:47:03 +0100

    refactor(app): #31 commented non importat features for release

--- apps/framr-web/lib/types/rule.d.ts
+++ apps/framr-web/lib/types/rule.d.ts
@@ -6,7 +6,11 @@ export interface CreateStandAloneRule {
   name?: string;
   tool: Tool;
   concernedDpoint: DPoint;
-  description: RuleEnum;
+  description:
+    | RuleEnum.SHOULD_BE_PRESENT
+    | RuleEnum.SHOULD_BE_THE_FIRST
+    | RuleEnum.SHOULD_NOT_BE_PRESENT
+    | RuleEnum.SHOULD_NOT_BE_THE_FIRST;
   framesets: FrameEnum[];
 }
 
@@ -16,6 +20,16 @@ export interface StandAloneRule extends CreateStandAloneRule {
 
 export interface CreateRuleWithOtherDPoint extends CreateStandAloneRule {
   otherDpoints: DPoint[];
+  description:
+    | RuleEnum.SHOULD_BE_FOLLOWED_BY_OTHER
+    | RuleEnum.SHOULD_BE_IMMEDIATELY_FOLLOWED_BY_OTHER
+    | RuleEnum.SHOULD_BE_IMMEDIATELY_PRECEDED_BY_OTHER
+    | RuleEnum.SHOULD_BE_PRECEDED_BY_OTHER
+    | RuleEnum.SHOULD_BE_PRESENT_AS_SET_ONLY
+    | RuleEnum.SHOULD_NOT_BE_FOLLOWED_BY_OTHER
+    | RuleEnum.SHOULD_NOT_BE_IMMEDIATELY_FOLLOWED_BY_OTHER
+    | RuleEnum.SHOULD_NOT_BE_IMMEDIATELY_PRECEDED_BY_OTHER
+    | RuleEnum.SHOULD_NOT_BE_PRECEDED_BY_OTHER;
 }
 
 export interface RuleWithOtherDPoint extends CreateRuleWithOtherDPoint {
@@ -25,30 +39,33 @@ export interface RuleWithOtherDPoint extends CreateRuleWithOtherDPoint {
 export interface CreateRuleWithConstraint extends CreateStandAloneRule {
   interval: number;
   type: ConstraintEnum;
+  description:
+    | RuleEnum.SHOULD_BE_PRESENT_WITH_DENSITY_CONSTRAINT
+    | RuleEnum.SHOULD_BE_PRESENT_WITH_UPDATE_RATE_CONSTRAINT;
 }
 
 export interface RuleWithConstraint extends CreateRuleWithConstraint {
   id: string;
 }
 
-export interface CreateRuleWithOccurenceConstraint
-  extends CreateStandAloneRule {
-  occurences: number;
-}
+// export interface CreateRuleWithOccurenceConstraint
+//   extends CreateStandAloneRule {
+//   occurences: number;
+// }
 
-export interface RuleWithOccurenceConstraint
-  extends CreateRuleWithOccurenceConstraint {
-  id: string;
-}
+// export interface RuleWithOccurenceConstraint
+//   extends CreateRuleWithOccurenceConstraint {
+//   id: string;
+// }
 
 export type CreateRule =
   | CreateStandAloneRule
   | CreateRuleWithConstraint
-  | CreateRuleWithOccurenceConstraint
+  // | CreateRuleWithOccurenceConstraint
   | CreateRuleWithOtherDPoint;
 
 export type Rule =
   | StandAloneRule
   | RuleWithConstraint
-  | RuleWithOccurenceConstraint
+  // | RuleWithOccurenceConstraint
   | RuleWithOtherDPoint;
