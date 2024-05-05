1d30927cfa9cd83faed6543a3363e3ca269a98b1
Marco Kuidja <kuidjamarco@gmail.com>
2024-05-05 23:20:46 +0100

    fix(service-layer): #34 wrote overall test for framr service

--- apps/framr-web/lib/services/core/modules/framr/specs/RulesHandler.spec.ts
+++ apps/framr-web/lib/services/core/modules/framr/specs/RulesHandler.spec.ts
@@ -1,27 +1,30 @@
+import { randomUUID } from 'crypto';
 import { EventBus } from '../../../../../services/libs/event-bus';
 import {
   DPoint,
-  FSLFrameType,
   GeneratorConfigRule,
+  MWDTool,
   Service,
 } from '../../../../../types';
-import { FrameEnum, ToolEnum } from '../../../../../types/enums';
+import {
+  FrameEnum,
+  ToolEnum,
+  WithOtherDPointRuleEnum,
+} from '../../../../../types/enums';
 import { IDBConnection } from '../../../db/IDBConnection';
 import { ToolsEventChannel } from '../../tools/ToolInterface';
 import { ToolsService } from '../../tools/ToolsService';
-import { DataProcessor } from '../../tools/data-processor/DataProcessor';
 import { FramrService } from '../FramrService';
 import { sampleFileContent } from './sample-file';
 
 describe('RulesHandler', () => {
   const eventBus = new EventBus();
   const database = IDBConnection.getDatabase();
-  const dataProcessor = new DataProcessor();
   const framr = new FramrService();
   const toolServices = new ToolsService();
 
-  const sampleDPoints: DPoint[] = [];
-  let sampleData: Service[] = [];
+  let sampleService: Service;
+  let sampleDPoints: DPoint[] = [];
   let sampleDataRules: GeneratorConfigRule[] = [];
 
   beforeAll(async () => {
@@ -38,38 +41,56 @@ describe('RulesHandler', () => {
     toolServices.createFrom(file);
     await createFromCallback;
 
-    sampleData = (await database.findAll('services'))
-      .map((_) => _.value)
-      .slice(0, 3);
-    sampleDataRules = (await database.findAll('rules'))
-      .filter((_) =>
-        sampleData.some((data) => data.tool.id === _.value.tool.id)
+    [sampleService] = (await database.findAll('services')).map((_) => _.value);
+    const rules = (await database.findAll('rules'))
+      .filter(
+        (_) =>
+          sampleService.tool.id === _.value.tool.id &&
+          _.value.framesets.includes(FrameEnum.ROT)
       )
       .map((_) => ({
         ..._.value,
         isActive: true,
         isGeneric: true,
-      }))
+      }));
+    sampleDataRules = rules.slice(0, 3);
+    sampleDPoints = (await database.findAll('dpoints'))
+      .filter((_) =>
+        sampleDataRules.some((rule) => rule.concernedDpoint.id === _.value.id)
+      )
+      .map((_) => _.value)
       .slice(0, 3);
+
+    const mwdTool: MWDTool = {
+      id: 'mwd-tool-id',
+      long: 'MWD Tool Id',
+      maxBits: 12,
+      maxDPoints: 5,
+      name: '',
+      type: ToolEnum.MWD,
+      version: 'v1.0',
+    };
 
     framr.initialize({
       bitRate: 5,
       MWDTool: {
-        id: '',
-        long: '',
-        maxBits: 12,
-        maxDPoints: 5,
-        name: '',
-        type: ToolEnum.MWD,
-        version: 'v1.0',
-        rules: sampleDataRules,
+        ...mwdTool,
+        rules: rules
+          .slice(3, 5)
+          .map((rule) => ({
+            ...rule,
+            tool: mwdTool,
+            concernedDpoint: { ...rule.concernedDpoint, tool: mwdTool },
+          })),
       },
       penetrationRate: 15,
-      tools: sampleData.map((_) => ({
-        ..._.tool,
-        type: ToolEnum.LWD,
-        rules: sampleDataRules.filter((rule) => rule.tool.id === _.tool.id),
-      })),
+      tools: [
+        {
+          ...sampleService.tool,
+          type: ToolEnum.LWD,
+          rules: sampleDataRules,
+        },
+      ],
     });
   });
 
@@ -89,31 +110,56 @@ describe('RulesHandler', () => {
 
   it('Should dispatch dpoint to various framesets', () => {
     const fslNumber = 1;
-
-    sampleData.forEach(({ tool, dpoints: dps }) => {
-      framr.addAndDispatchDPoints(fslNumber, tool.id, dps);
-      sampleDPoints.push(...dps);
-    });
+    const { tool, dpoints } = sampleService;
+    framr.addAndDispatchDPoints(fslNumber, tool.id, dpoints);
+    sampleDPoints.push(...dpoints);
     const fsl = framr.generatorConfig?.framesets.fsl.find(
       (_) => _.number === fslNumber
     );
     expect(fsl).not.toBeNull();
 
-    for (const frame in fsl?.framesets) {
-      const fslFrameset = fsl?.framesets[frame as FSLFrameType];
-      expect(fslFrameset.dpoints.length).toBeGreaterThan(0);
-    }
+    const fslFrameset = fsl?.framesets[FrameEnum.ROT];
+    expect(fslFrameset).not.toBeUndefined();
+    expect(fslFrameset?.dpoints.length).toBeGreaterThan(0);
   });
 
   it('Should order framset dpoints', () => {
     const fslNumber = 1;
-    if (sampleDPoints.length === 0) {
-      sampleData.forEach(({ tool, dpoints: dps }) => {
-        framr.addAndDispatchDPoints(fslNumber, tool.id, dps);
-        sampleDPoints.push(...dps);
-      });
-    }
+    const { tool } = sampleService;
+    framr.addAndDispatchDPoints(fslNumber, tool.id, sampleDPoints);
+
+    const cdpIndex = Math.floor(Math.random() * sampleDPoints.length);
+    const concernedDpoint = sampleDPoints[cdpIndex];
+    const description =
+      cdpIndex % 2
+        ? WithOtherDPointRuleEnum.SHOULD_BE_IMMEDIATELY_FOLLOWED_BY_OTHER
+        : cdpIndex % 3
+        ? WithOtherDPointRuleEnum.SHOULD_BE_IMMEDIATELY_PRECEDED_BY_OTHER
+        : sampleDPoints.length == 3
+        ? WithOtherDPointRuleEnum.SHOULD_BE_PRESENT_AS_SET_ONLY
+        : WithOtherDPointRuleEnum.SHOULD_NOT_BE_PRECEDED_BY_OTHER;
+    const otherDpoints = sampleDPoints.slice(cdpIndex + 1);
+
+    framr.updateToolRules(tool.id, [
+      ...sampleDataRules,
+      {
+        tool,
+        description,
+        concernedDpoint,
+        otherDpoints,
+        isActive: true,
+        isGeneric: false,
+        id: randomUUID(),
+        framesets: [FrameEnum.ROT],
+      },
+    ]);
 
     framr.orderFramesetDPoints(fslNumber, FrameEnum.ROT);
+
+    console.log(framr.orderedDPoints);
+    console.log({ description, concernedDpoint, otherDpoints, sampleDPoints });
+
+    const rules = framr.getRules(tool.id);
+    expect(rules.length).toEqual(sampleDataRules.length + 1);
   });
 });
