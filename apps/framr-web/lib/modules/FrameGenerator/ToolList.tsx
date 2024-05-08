b34982891a94d04b4ee4d73b43337737d692146f
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-05-08 09:17:44 +0100

    feat(app): #31 adjusted toollist to manage all tools and selected dpoints

--- apps/framr-web/lib/modules/FrameGenerator/ToolList.tsx
+++ apps/framr-web/lib/modules/FrameGenerator/ToolList.tsx
@@ -1,24 +1,25 @@
-import right from '@iconify/icons-fluent/chevron-right-20-regular';
 import SearchIcon from '@iconify/icons-fluent/search-32-regular';
-import square from '@iconify/icons-fluent/square-24-regular';
 import { Icon } from '@iconify/react';
-import {
-    Box,
-    Checkbox,
-    IconButton,
-    InputAdornment,
-    TextField,
-    Tooltip,
-    Typography,
-} from '@mui/material';
+import { Box, InputAdornment, TextField } from '@mui/material';
 import Scrollbars from 'rc-scrollbars';
-import { GeneratorConfig, IGeneratorConfigTool } from '../../types';
+import { useEffect, useState } from 'react';
+import { DPoint, GeneratorConfig, IGeneratorConfigTool } from '../../types';
+import Tool from './Tool';
 
 interface ToolListProps {
   data: GeneratorConfig;
+  getDPoints: (selectedDPoints: DPoint[]) => void;
 }
-export default function ToolList({ data: { MWDTool, tools } }: ToolListProps) {
+export default function ToolList({
+  data: { MWDTool, tools },
+  getDPoints,
+}: ToolListProps) {
   const toolList: IGeneratorConfigTool[] = [MWDTool, ...tools];
+  const [selectedDPoints, setSelectedDPoints] = useState<DPoint[]>([]);
+
+  useEffect(() => {
+    getDPoints(selectedDPoints);
+  }, [getDPoints, selectedDPoints]);
   return (
     <Box
       sx={{
@@ -47,37 +48,22 @@ export default function ToolList({ data: { MWDTool, tools } }: ToolListProps) {
         }}
       />
       <Scrollbars autoHide universal>
-        {toolList.map(({ name }, index) => (
-          <Box key={index}>
-            <Box
-              sx={{
-                display: 'grid',
-                gridTemplateColumns: 'auto 1fr auto',
-                columnGap: 0.5,
-                alignItems: 'center',
-                cursor: 'pointer',
-                borderRadius: 0.5,
-                p: 0.5,
-                borderBottom: '1px solid #D1D5DB',
-                '&:hover': {
-                  backgroundColor: '#F3F4F6',
-                },
-              }}
-            >
-              <Checkbox
-                size="small"
-                icon={<Icon icon={square} fontSize={22} color="#D1D5DB" />}
-              />
-              <Typography fontWeight={700} variant="body2">
-                {name}
-              </Typography>
-              <Tooltip arrow title={'show services'}>
-                <IconButton size="small">
-                  <Icon icon={right} />
-                </IconButton>
-              </Tooltip>
-            </Box>
-          </Box>
+        {toolList.map((tool, index) => (
+          <Tool
+            key={index}
+            tool={tool}
+            getDPoints={(selectedDPoints, prevSelectedDPoints) =>
+              setSelectedDPoints((prev) => {
+                const tt = prev.filter((dpoint) => {
+                  return !prevSelectedDPoints
+                    .map(({ id }) => id)
+                    .includes(dpoint.id);
+                });
+
+                return [...selectedDPoints, ...tt];
+              })
+            }
+          />
         ))}
       </Scrollbars>
     </Box>
