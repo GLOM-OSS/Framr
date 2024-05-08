772543f6d027c67c59eddc7b72b79b56abb90040
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-05-08 11:57:36 +0100

    feat(app): #31 configured app to work with header

--- apps/framr-web/pages/index.tsx
+++ apps/framr-web/pages/index.tsx
@@ -1,14 +1,23 @@
+import download from '@iconify/icons-fluent/arrow-download-20-regular';
+import { Icon } from '@iconify/react';
 import { Box, Button, Divider } from '@mui/material';
 import { useEffect, useState } from 'react';
 import GeneratorHeader from '../lib/modules/FrameGenerator/GeneratorHeader';
 import ToolList from '../lib/modules/FrameGenerator/ToolList';
+import FramesetHeader from '../lib/modules/FrameGenerator/framesets/FramesetHeader';
+import FramesetList from '../lib/modules/FrameGenerator/framesets/FramesetList';
 import FrameGeneratorConfig from '../lib/modules/FrameGeneratorConfig/FrameGeneratorConfig';
 import { DPoint, GeneratorConfig } from '../lib/types';
+import { FrameEnum } from '../lib/types/enums';
 
 export default function FrameGenerator() {
   const [isConfigOpen, setIsConfigOpen] = useState(true);
   const [frameConfig, setFrameConfig] = useState<GeneratorConfig>();
   const [selectedDPoints, setSelectedDPoints] = useState<DPoint[]>([]);
+  const [activeFSL, setActiveFSL] = useState<number>(1);
+  const [selectedFrames, setSelectedFrames] = useState<FrameEnum[]>([]);
+
+  const [isSelectMode, setIsSelectMode] = useState<boolean>(false);
 
   useEffect(() => {
     /*TODO: CALL API HERE THAT SORTS SELECTED DPOINTS
@@ -51,7 +60,30 @@ export default function FrameGenerator() {
           getDPoints={(selectedDPoints) => setSelectedDPoints(selectedDPoints)}
         />
         <Divider orientation="vertical" />
-        <Button onClick={() => setIsConfigOpen(true)}>Frame Generator</Button>
+        <Box sx={{ display: 'grid', gridTemplateRows: 'auto 1fr auto' }}>
+          <FramesetHeader
+            activeFSL={activeFSL}
+            selectedFrames={selectedFrames}
+            setActiveFSL={setActiveFSL}
+            setSelectedFrames={setSelectedFrames}
+            isSelectMode={isSelectMode}
+            setIsSelectMode={setIsSelectMode}
+            submitNewDPoint={(val) =>
+              setSelectedDPoints((prev) => [...prev, val])
+            }
+            handleRemoveConstraints={() => alert('can remove constraints')}
+            handleRemoveDPoints={() => alert('Remove selected dpoints')}
+          />
+          <FramesetList />
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ justifySelf: 'end' }}
+            startIcon={<Icon icon={download} />}
+          >
+            Export Framesets
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
