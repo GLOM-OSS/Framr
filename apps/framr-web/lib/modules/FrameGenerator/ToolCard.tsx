7b2dc3653b7a2d5fd76523e21e031be9a7f74e5d
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-05-08 09:14:49 +0100

    feat(app): #31 adjusted tool card to be used for services display

--- apps/framr-web/lib/modules/FrameGenerator/ToolCard.tsx
+++ apps/framr-web/lib/modules/FrameGenerator/ToolCard.tsx
@@ -1,44 +1,56 @@
+import down from '@iconify/icons-fluent/chevron-down-20-regular';
 import right from '@iconify/icons-fluent/chevron-right-20-regular';
 import square from '@iconify/icons-fluent/square-24-regular';
 import { Icon } from '@iconify/react';
 import { Box, Checkbox, IconButton, Tooltip, Typography } from '@mui/material';
 
-interface ToolCardProps {
+export function ToolCard({
+  name,
+  isOpen = false,
+  isSelected,
+  handleSelect,
+  handleOpen,
+}: {
   name: string;
+  isOpen?: boolean;
   isSelected: boolean;
   handleSelect: () => void;
-}
-export default function ToolCard({ name }: ToolCardProps) {
+  handleOpen?: () => void;
+}) {
   return (
-    <Box>
-      <Box
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'auto 1fr auto',
-          columnGap: 0.5,
-          alignItems: 'center',
-          cursor: 'pointer',
-          borderRadius: 0.5,
-          p: 0.5,
-          borderBottom: '1px solid #D1D5DB',
-          '&:hover': {
-            backgroundColor: '#F3F4F6',
-          },
-        }}
-      >
-        <Checkbox
-          size="small"
-          icon={<Icon icon={square} fontSize={22} color="#D1D5DB" />}
-        />
-        <Typography fontWeight={700} variant="body2">
-          {name}
-        </Typography>
-        <Tooltip arrow title={'show services'}>
-          <IconButton size="small">
-            <Icon icon={right} />
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: 'auto 1fr auto',
+        columnGap: 0.5,
+        alignItems: 'center',
+        cursor: 'pointer',
+        borderRadius: 0.5,
+        p: 0.5,
+        borderBottom: '1px solid #D1D5DB',
+        backgroundColor: handleOpen ? '#FDFDFD' : 'none',
+        '&:hover': {
+          backgroundColor: '#F3F4F6',
+        },
+      }}
+      onClick={() => (handleOpen ? null : handleSelect())}
+    >
+      <Checkbox
+        size="small"
+        checked={isSelected}
+        onChange={() => (handleOpen ? handleSelect() : null)}
+        icon={<Icon icon={square} fontSize={20} color="#D1D5DB" />}
+      />
+      <Typography fontWeight={700} variant="body2">
+        {name}
+      </Typography>
+      {handleOpen && (
+        <Tooltip arrow title={isOpen ? 'Hide services' : 'Show services'}>
+          <IconButton size="small" onClick={handleOpen}>
+            <Icon icon={isOpen ? down : right} />
           </IconButton>
         </Tooltip>
-      </Box>
+      )}
     </Box>
   );
 }
