d7cdf5a678c095bb2a7c5d950c38bb8170a9bc70
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-05-11 08:30:32 +0100

    feat(app): #31 added usePWA Lib

--- libs/usePWA/src/lib/usePWA.spec.tsx
+++ libs/usePWA/src/lib/usePWA.spec.tsx
@@ -1,10 +1,37 @@
 import { render } from '@testing-library/react';
+import { InstallPWAContextProvider, usePWA } from '../index';
 
-import UsePWA from './usePWA';
+// Mock window.matchMedia
+describe('usePWA', () => {
+  let matchMediaMock: jest.Mock;
 
-describe('UsePWA', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<UsePWA />);
+  beforeEach(() => {
+    matchMediaMock = jest.fn();
+    window.matchMedia = matchMediaMock;
+  });
+
+  afterEach(() => {
+    matchMediaMock.mockClear();
+  });
+
+  it('should be used successfully', () => {
+    // Mock the return value of window.matchMedia
+    matchMediaMock.mockReturnValue({
+      matches: true,
+      addListener: jest.fn(),
+    });
+
+    const UsingPWA = () => {
+      const { isInstallable } = usePWA();
+      return <div>{isInstallable}</div>;
+    };
+
+    const { baseElement } = render(
+      <InstallPWAContextProvider>
+        <UsingPWA />
+      </InstallPWAContextProvider>
+    );
+
     expect(baseElement).toBeTruthy();
   });
 });
